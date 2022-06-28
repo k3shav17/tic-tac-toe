dd4d497d782ab267c51fad7f792a7566991de51a
k3shav17 <keshavrao0489@gmail.com>
2022-06-28 10:35:23 +0530

    tic-tac-toe with angular, functionality has been done

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+import { NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+
+@NgModule({
+  declarations: [AppComponent, SquareComponent, BoardComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NbLayoutModule,
+    NbEvaIconsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
